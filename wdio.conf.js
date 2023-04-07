c5fc259b05dfd67449b50336e4a23414fa840815
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-07 10:45:23 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -290,7 +290,7 @@ exports.config = {
     // },
     /**
     * Gets executed when a refresh happens.
-    * @param {String} oldSessionId session ID of the old session
+    * @param {String} oldSessionId session ID of the old session, OLD session
     * @param {String} newSessionId session ID of the new session
     */
     //onReload: function(oldSessionId, newSessionId) {

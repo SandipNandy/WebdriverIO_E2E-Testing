a989567084c3d13185433bfb8c5b8324fbfdb6a7
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-04 10:58:42 +0530

    Update wdio.uat.conf.js

--- wdio.uat.conf.js
+++ wdio.uat.conf.js
@@ -1,13 +1,9 @@
  //merge parent conf object + add new changes in uat conf(baseurl, connectiontimeour) for UAT env
 const merge = require('deepmerge')
 const wdioConf = require('./wdio.conf.js')
-
-
 exports.config = merge(wdioConf.config, {
-
 baseUrl : 'http://automationpractice.com/',
 waitforTimeout: 5000,
-
 mochaOpts: {
     ui: 'bdd',
     timeout: 60000,

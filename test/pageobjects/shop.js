b91d59a9ea949ddb691a5c6249a78267c834d055
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-04 11:00:34 +0530

    Update shop.js

--- test/pageobjects/shop.js
+++ test/pageobjects/shop.js
@@ -6,14 +6,11 @@ get checkout()
 }
 get cards()
 { 
-
-    
-    return $$("div[class='card h-100']")
+ return $$("div[class='card h-100']")
 }
 
  async addProductToCart(products)
  {
-
     for( let i =0; i< await this.cards.length;i++)
     {
         const card = await this.cards[i].$("div h4 a")

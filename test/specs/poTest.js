f41cbd16f85d0eafc488d0039914adc98a909649
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-31 13:05:57 +0530

    Update poTest.js

--- test/specs/poTest.js
+++ test/specs/poTest.js
@@ -7,6 +7,7 @@ const reviewPage =require('../pageobjects/reviewpage')
 const expectchai = require('chai').expect
 
 const fs =require('fs')
+
 let credentials =JSON.parse(fs.readFileSync('test/testData/LoginTest.json'))
 let e2eCredentials =JSON.parse(fs.readFileSync('test/testData/e2eTest.json'))
 

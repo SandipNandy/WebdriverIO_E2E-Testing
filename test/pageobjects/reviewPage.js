72c5bb2c3cad3fc0a05f11ffe25ffa120f35fc27
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-31 13:03:28 +0530

    Update reviewPage.js

--- test/pageobjects/reviewPage.js
+++ test/pageobjects/reviewPage.js
@@ -2,14 +2,17 @@ class ReviewPage
 {   
 get productPrices()
 {  
+    
     return $$("//tr/td[4]/strong")
 }
 get totalPrice()
 { 
+    
     return $("h3 strong")
 }
 async sumOfProducts()
- {  
+ { 
+     
     const sumOfProducts = (await Promise.all(await this.productPrices.map(async (productPrice)=> parseInt((await productPrice.getText()).split(".")[1].trim()))))
     .reduce((acc,price)=>acc+price,0)//0+ 13000 =13000    50000+13000 =
     console.log(sumOfProducts)

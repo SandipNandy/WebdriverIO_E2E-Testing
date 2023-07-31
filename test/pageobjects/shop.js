f9ee2dc03df31a81bf8ba50b966c2ba55f5a9c4f
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-31 13:03:47 +0530

    Update shop.js

--- test/pageobjects/shop.js
+++ test/pageobjects/shop.js
@@ -2,10 +2,12 @@ class Shop
 {    
 get checkout()
 {
+     
      return $("*=Checkout")
 }
 get cards()
-{   
+{  
+     
  return $$("div[class='card h-100']")
 }
  async addProductToCart(products)

bc94f2239d6ddb07379941b9253b7f06de1f2aa9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-16 14:05:42 +0530

    Update windowsFrames.js

--- test/specs/windowsFrames.js
+++ test/specs/windowsFrames.js
@@ -2,12 +2,9 @@ describe('Windows and Frames Miscellanous',async()=>
          {
                   
     it('Parent and Child windows switch',async()=>
-   {
-            
-        await  browser.url("/AutomationPractice/")
-            
-        await $(".blinkingText").click()
-            
+   {  
+        await  browser.url("/AutomationPractice/")  
+        await $(".blinkingText").click()   
        const handles = await browser.getWindowHandles()// 2 windows -
             
        await browser.switchToWindow(handles[1])

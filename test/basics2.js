4cf7b237ed2019aa23b8e429fad86f47cc293af3
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-11 11:13:59 +0530

    Update basics2.js

--- test/basics2.js
+++ test/basics2.js
@@ -11,7 +11,7 @@ module.exports =class Person
     }
     fullName()
     {
-         console.log(this.firstName+this.lastName)
+      console.log(this.firstName+this.lastName)
     }
 }
 // let name= new Person("Tim","joseph")

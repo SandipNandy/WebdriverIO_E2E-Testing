8f4e9c3219f4b987a0c81440ba61e36f5a0b161c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-05-25 13:25:07 +0530

    Update poTest.js

--- test/specs/poTest.js
+++ test/specs/poTest.js
@@ -1,6 +1,7 @@
 const loginPage =require('../pageobjects/loginPage')
 
 const shopPage =require('../pageobjects/shop')
+
 const reviewPage =require('../pageobjects/reviewpage')
 
 const expectchai = require('chai').expect

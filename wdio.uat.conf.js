5e494d5b89c72acfef17e0d922596ace105f457d
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-21 10:33:09 +0530

    Update wdio.uat.conf.js

--- wdio.uat.conf.js
+++ wdio.uat.conf.js
@@ -1,6 +1,7 @@
  //merge parent conf object + add new changes in uat conf(baseurl, connectiontimeour) for UAT env
 
 const merge = require('deepmerge')
+
 const wdioConf = require('./wdio.conf.js')
 exports.config = merge(wdioConf.config, {
 baseUrl : 'http://automationpractice.com/',

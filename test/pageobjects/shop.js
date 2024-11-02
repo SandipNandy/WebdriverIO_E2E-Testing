8f4adb33921c9d9336027827950c9e72c4f4a275
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-11-02 13:07:19 +0530

    Update shop.js

--- test/pageobjects/shop.js
+++ test/pageobjects/shop.js
@@ -12,7 +12,6 @@ get cards()
  {
     for( let i =0; i< await this.cards.length;i++)
     {
-       
          const card = await this.cards[i].$("div h4 a")
        if(products.includes(await card.getText()))
        {

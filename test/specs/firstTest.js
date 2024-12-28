39012fe54ff7413725c696e67f00006946b7accd
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-12-28 19:14:24 +0530

    Update firstTest.js

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -9,6 +9,7 @@ describe('Ecommerce Application',async()=>
      await   browser.url("/loginpagePractise/")
     
     console.log(await browser.getTitle())
+    
     await expect(browser).toHaveTitleContaining("SN Academy")
     //Css Selector, Xpath
     await $("input[name='username']").setValue("snacademy")

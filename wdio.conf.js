f31dd6c480b5fe640016eaaee141e4abd4efe8a2
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-04-25 12:44:52 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -6,7 +6,6 @@ exports.config = {
     // ====================
     // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
     // on a remote machine).
-    
     runner: 'local',
     // ==================
     // Specify Test Files

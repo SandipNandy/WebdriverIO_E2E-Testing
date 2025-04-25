d863ba1f5cba514c1b81c9a3def7a1537c6ccd54
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-04-25 12:44:37 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -11,7 +11,6 @@ exports.config = {
     // ==================
     // Specify Test Files
     // ==================
-    
     // Define which test specs should run. The pattern is relative to the directory
     // from which `wdio` was called. Notice that, if you are calling `wdio` from an
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working

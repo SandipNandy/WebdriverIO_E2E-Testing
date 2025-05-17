d42cc94d1ab085dd769b85ed76d5718a06678bd2
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-17 08:33:44 +0530

    Update shop.js

--- test/pageobjects/shop.js
+++ test/pageobjects/shop.js
@@ -15,8 +15,7 @@ get cards()
     for( let i =0; i< await this.cards.length;i++)
     { const card = await this.cards[i].$("div h4 a")
        if(products.includes(await card.getText()))
-       {
-            await this.cards[i].$(".card-footer button").click()
+       { await this.cards[i].$(".card-footer button").click()
 
        }
     }
